be1fedb48921cf95ceef98f64a3fb58733207014
Daniel Skowroński <daniel.skowronski@grandparade.co.uk>
2021-02-12 12:44:12 +0100

    support multiple search paths in AD, define OrgMember class and merge all results into dict of OrgMember for later parsing

--- adot.py
+++ adot.py
@@ -3,11 +3,20 @@ from getpass import getpass
 from ldap3 import Server, Connection, SAFE_SYNC
 from pprint import pprint
 
+class OrgMember:
+	def __init__(self, dn, cn, email, manager_dn):
+		self.dn=dn
+		self.cn=cn
+		self.email=email
+		self.manager_dn=manager_dn
+	def __str__(self):
+		return "User:      %s\n  CN:      %s\n  Mailbox: %s\n  Manager: %s" % (self.dn, self.cn, self.email, self.manager_dn)
+
 parser = argparse.ArgumentParser(description='AD Org Tree')
 parser.add_argument('server',  type=str)
 parser.add_argument('domain',  type=str)
 parser.add_argument('username',type=str)
-parser.add_argument('path',    type=str)
+parser.add_argument('--path',  type=str, nargs='+')
 args = parser.parse_args()
 
 password=keyring.get_password('adot_ldap_pass',args.username)
@@ -17,5 +26,19 @@ if password==None:
 
 server = Server(args.server)
 conn = Connection(server, "%s\\%s"%(args.domain,args.username), password, client_strategy=SAFE_SYNC, auto_bind=True)
-status, result, response, _ = conn.search(args.path, '(objectclass=person)')  
-pprint(response)
+
+orgMembers={}
+
+for path in args.path:
+	print("Querying %s" % (path))
+	status, result, people, _ = conn.search(path, '(objectclass=user)', attributes=['distinguishedName','manager','cn','sAMAccountName','otherMailbox'])  
+	for person in people:
+		if len(person['attributes']['otherMailbox'])>0:
+			mailbox=person['attributes']['otherMailbox'][0]
+		else:
+			mailbox=''
+		orgMember=OrgMember(person['dn'],person['attributes']['cn'],mailbox,person['attributes']['manager'])
+		orgMembers.update({person['dn']:orgMember})
+
+for dn,orgMember in orgMembers.items():
+	print(orgMember)
